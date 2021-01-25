37600bc3657dccfc81b1bd08f09c05d7c2bab985
Sho Nakatani <lay.sakura@gmail.com>
2021-01-25 12:55:18 +0900

    require_version in __init__.py

--- actfw_gstreamer/__init__.py
+++ actfw_gstreamer/__init__.py
@@ -1,33 +1,6 @@
 from ._version import __version__
 
 
-def init():
-    """Initializes GObject thread and GStreamer.
-
-    An application using actfw-gstreamer must call this function at first.
-
-    Examples:
-
-        >>> import actfw_gstreamer
-        >>> actfw_gstreamer.init()
-        >>>
-        >>> # foobar module might use Gst / GObject.
-        >>> # Initialization must be finished before importing any other packages using Gst / GObject.
-        >>> import foobar
-        >>>
-        >>> if __name__ == '__main__':
-        >>>     # ...
-    """
-    if not init.initialized:
-        import gi
-        gi.require_version('Gst', '1.0')
-        gi.require_version('GstVideo', '1.0')
-
-        from gi.repository import Gst, GObject
-        GObject.threads_init()
-        Gst.init(None)
-
-        init.initialized = True
-
-
-init.initialized = False
+import gi
+gi.require_version('Gst', '1.0')
+gi.require_version('GstVideo', '1.0')
