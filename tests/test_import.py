7759508cbf01eecedfddeeceb16428747fb8589d
keno <keno.ss57@gmail.com>
2021-04-01 19:03:24 +0900

    poetry run pysen run format

--- tests/test_import.py
+++ tests/test_import.py
@@ -5,4 +5,4 @@ from nose2.tools import params
     # {'from': 'actfw_gstreamer.foo', 'import': 'Bar'},
 )
 def test_import_actfw_gstreamer(param):
-    exec(f'''from {param['from']} import {param['import']}''')
+    exec(f"""from {param['from']} import {param['import']}""")
