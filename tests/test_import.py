566fe9de4cca65be429115e07871d12a46f3800f
keno <keno.ss57@gmail.com>
2021-04-28 15:38:55 +0900

    refactor: nose2 -> pytest

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,35 +1,17 @@
-from nose2.tools import params
+import pytest
 
 
-@params(
-    {
-        "from": "actfw_gstreamer.capture",
-        "import": "GstreamerCapture",
-    },
-    {
-        "from": "actfw_gstreamer.gstreamer.pipeline",
-        "import": "PipelineBuilder, PipelineGenerator",
-    },
-    {
-        "from": "actfw_gstreamer.gstreamer.converter",
-        "import": "ConverterBase, ConverterRaw, ConverterPIL",
-    },
-    {
-        "from": "actfw_gstreamer.gstreamer.exception",
-        "import": "GstNotInitializedError, PipelineBuildError, ConnectionLostError",
-    },
-    {
-        "from": "actfw_gstreamer.gstreamer.preconfigured_pipeline",
-        "import": "videotestsrc, rtsp_h264",
-    },
-    {
-        "from": "actfw_gstreamer.gstreamer.stream",
-        "import": "GstStreamBuilder",
-    },
-    {
-        "from": "actfw_gstreamer.restart_handler",
-        "import": "RestartAction, Stop, Restart, RestartHandlerBase, SimpleRestartHandler",
-    },
+@pytest.mark.parametrize(
+    "from_, import_",
+    [
+        ("actfw_gstreamer.capture", "GstreamerCapture"),
+        ("actfw_gstreamer.gstreamer.pipeline", "PipelineBuilder, PipelineGenerator"),
+        ("actfw_gstreamer.gstreamer.converter", "ConverterBase, ConverterRaw, ConverterPIL"),
+        ("actfw_gstreamer.gstreamer.exception", "GstNotInitializedError, PipelineBuildError, ConnectionLostError"),
+        ("actfw_gstreamer.gstreamer.preconfigured_pipeline", "videotestsrc, rtsp_h264"),
+        ("actfw_gstreamer.gstreamer.stream", "GstStreamBuilder"),
+        ("actfw_gstreamer.restart_handler", "RestartAction, Stop, Restart, RestartHandlerBase, SimpleRestartHandler"),
+    ],
 )
-def test_import_actfw_gstreamer(param):
-    exec(f"""from {param['from']} import {param['import']}""")
+def test_import_actfw_gstreamer(from_, import_):
+    exec(f"""from {from_} import {import_}""")
