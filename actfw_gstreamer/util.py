78d76b8b75eb08d1ea423e7c0a73d5244206c1de
amutake <amutake.s@gmail.com>
2024-01-24 13:28:04 +0900

    Fix mypy errors

--- actfw_gstreamer/util.py
+++ actfw_gstreamer/util.py
@@ -11,10 +11,10 @@ def _get_gst() -> "Gst":  # type: ignore  # noqa F821
 
     if CACHED_GST is None:
         try:
-            import gi
+            import gi # type: ignore[import]
 
             gi.require_version("Gst", "1.0")
-            from gi.repository import Gst
+            from gi.repository import Gst # type: ignore[import]
 
             CACHED_GST = Gst
             if not Gst.is_initialized():
